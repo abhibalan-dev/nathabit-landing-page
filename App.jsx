1335c5f88331b358474f371318dd166b16c4a484
abhibalan-dev <abhayabalan12@gmail.com>
2026-06-02 20:18:01 +0530

    upload project files

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Products />
+      <Testimonials />
+    </>
+  );
+}
+
+export default App;
